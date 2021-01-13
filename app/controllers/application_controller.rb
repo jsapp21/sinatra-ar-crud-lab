7fb01b461a06768236e8b38510f89609c7f93735
James Sapp <jameseisapp@gmail.com>
2021-01-13 12:10:25 -0600

    commit tree c52be11ab0496fde50d13cd3c6e1471c01845056

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,49 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  #index
+  get '/articles' do
+    @articles = Article.all
+    erb :index  
+  end
+
+  #new article
+  get '/articles/new' do 
+    erb :new
+  end
+
+  # show/find
+  get '/articles/:id' do 
+    @articles = Article.find(params[:id])
+    erb :show 
+  end
+
+  #create new article 
+  post '/articles' do
+    @articles = Article.create(params)
+    redirect "/articles/#{@articles.id}"
+  end
+  
+  #edit article by id
+  get '/articles/:id/edit' do 
+    @articles = Article.find(params[:id])
+    erb :edit 
+  end
+
+  # update/save edited article
+  patch '/articles/:id' do
+    params.delete("_method")
+    @articles = Article.find(params[:id])
+    @articles.update(params)
+    redirect "/articles/#{@articles.id}"
+  end
+
+  # delete article by id 
+  delete '/articles/:id' do 
+    @articles = Article.delete(params[:id])
+    redirect "/articles"
+  end
+
+
 end
